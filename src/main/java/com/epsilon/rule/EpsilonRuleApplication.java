bf8b4c96c936b34f7d43935c6d48a5a8670b63ca
Breezy Frost <breezyfrost@163.com>
2024-02-18 00:34:27 +0800

    first commit

---
+++ src/main/java/com/epsilon/rule/EpsilonRuleApplication.java
@@ -0,0 +1,13 @@
+package com.epsilon.rule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpsilonRuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpsilonRuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epsilon/rule/EpsilonRuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epsilon.rule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpsilonRuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
